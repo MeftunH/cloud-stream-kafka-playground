630928f815d4ba61b6829140e65602184ff362bf
MeftunH <maftunhashimli@gmail.com>
2024-04-11 18:23:03 +0300

    Initial commit

---
+++ src/test/java/com/app/cloudstreamkafkaplayground/CloudStreamKafkaPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.cloudstreamkafkaplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudStreamKafkaPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
